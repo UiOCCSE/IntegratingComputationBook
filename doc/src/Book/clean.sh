40b498f086d75a03350f95eaaa0551e8e6fc010b
mhjensen <morten.hjorth-jensen@fys.uio.no>
2018-02-25 22:06:33 -0500

    Started with chapters for the Book

--- doc/src/Book/clean.sh
+++ doc/src/Book/clean.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 doconce clean
-rm -rf *.pdf *.tex ipynb*.tar.gz *.html ._*.html *~ reveal.js Trash README.txt
+rm -rf *.log *.aux *.out ipynb*.tar.gz *.html ._*.html *~ reveal.js Trash README.txt
